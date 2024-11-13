6a05c75a8b4b5ac0788d144d3e0c48ac8d06860e
Oriku Precious <bamideleprecious85@gmail.com>
2024-11-13 01:23:21 -0800

    Update ttwitter.py

--- ttwitter.py
+++ ttwitter.py
@@ -1,84 +1,147 @@
 import tkinter as tk
 from tkinter import scrolledtext
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+import urllib.parse
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.by import By
 import threading
+import pytz
+from tzlocal import get_localzone
+import os
+import sys
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
+    return os.path.join(base_path, relative_path)
+
+# Ashhlyynxo
+# Ayelen@123
 
 
 # Function to handle the Selenium scraping in a separate thread
 def start_scraping():
     query = query_input.get()
+    local_timezone = get_localzone()
     country = country_input.get()
     hours = int(hours_input.get())
-    interval = hours * 3600
-
-    # Set options for Selenium
+    current_date = datetime.now()
+    current_time = datetime.now()
+    until = datetime.now(local_timezone)
+    start_time = current_time - timedelta(hours=hours)
+    search_query = f"https://x.com/search?f=top&q={query}+{country}&src=typed_query"
     options = Options()
     options.headless = False  # Set to True if you want it to run without opening a browser window
-    s=Service('C:/Users/ACER/Downloads/chromedriver.exe')
+    chromedriver_path = resource_path('chromedriver.exe')
+
+    s = Service(chromedriver_path)
     driver = webdriver.Chrome(service=s)
 
-    URL = "https://x.com/i/flow/login"
+
     login = "https://x.com/i/flow/login"
     username = username_input.get()
     password = password_input.get()
 
-    while True:
-        try:
-            # Read cookies from a file if they exist
-            with open(f'{username}.txt', 'r') as f:
-                cookies = eval(f.read())
+    try:
+        cookies_file_path = resource_path(f'Logs/{username}.txt')
 
-            # Navigate to the search query URL
-            driver.get(f"https://x.com/search?q={query} {country}&src=typed_query")
+        # Open and read the cookies file
+        with open(cookies_file_path, 'r') as f:
+            cookies = eval(f.read())
+        time.sleep(10)
+        driver.get(f"https://x.com/")
 
-            # Add cookies to the driver
-            for cookie in cookies:
-                driver.add_cookie(cookie)
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+
+        driver.get(search_query)
+
+        time.sleep(10)
+        current_url = driver.current_url
+
+
+        if current_url == search_query:
             driver.refresh()
             time.sleep(5)
 
-            # Parse the page with BeautifulSoup
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            aria_labels = soup.find_all('div', {'role': 'group'})
+            time.sleep(20)
             all_data = []
+            scroll_limit = 5  # Adjust this limit based on how many scrolls you want
 
-            for div in aria_labels:
-                aria_label = div.get('aria-label')
-                if aria_label:
-                    data = {}
-                    for item in aria_label.split(', '):
-                        value, key = item.split(' ')
-                        if key in ['replies', 'reposts', 'likes', 'bookmarks', 'views']:
-                            data[key] = value
+            for _ in range(scroll_limit):
+                # Use BeautifulSoup to parse page content
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                aria_labels = soup.find_all('div', {'role': 'group'})
 
-                    parent_div = div.find_parent('article')
-                    if parent_div:
-                        post_link = parent_div.find('a', {'role': 'link', 'href': True,
-                                                          'href': lambda href: '/status/' in href})
-                        if post_link:
-                            post_url = post_link.get('href')
-                            full_url = f"https://twitter.com{post_url}"
-                            data['post_url'] = full_url
-                        else:
-                            data['post_url'] = 'No link found'
-                    all_data.append(data)
-                    print(all_data)
+                for div in aria_labels:
+                    aria_label = div.get('aria-label')  # Get the aria-label attribute from each div
+                    if aria_label:  # Ensure aria-label exists
+                        data = {}
+                        for item in aria_label.split(', '):
+                            value, key = item.split(' ')
+                            if key in ['replies', 'reposts', 'likes', 'bookmarks', 'views']:
+                                data[key] = value
+                            else:
+                                try:
+                                    data[key] = int(value.replace('K', '000').replace('M', '000000'))
+                                except ValueError:
+                                    data[key] = 0  # Fallback in case of conversion error
 
-            # Display the data in the output box
+                        # Find post URL within parent div
+                        parent_div = div.find_parent('article')
+                        if parent_div:
+                            post_link = parent_div.find('a', {'role': 'link', 'href': True,
+                                                              'href': lambda href: '/status/' in href})
+                            data[
+                                'post_url'] = f"https://twitter.com{post_link['href']}" if post_link else 'No link found'
+
+                            # Now handle timestamp and filtering based on the hours
+                            time_element = parent_div.find('time')
+                            if time_element and time_element.get('datetime'):
+                                try:
+                                    tweet_time_str = time_element['datetime']
+                                    tweet_time = datetime.strptime(tweet_time_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+
+                                    utc_time = pytz.utc.localize(tweet_time)
+
+                                    tweet_times = utc_time.astimezone(local_timezone)
+
+                                    # Calculate the time difference
+                                    time_difference = current_time - tweet_times.replace(tzinfo=None)
+
+
+                                    hours_difference = time_difference.total_seconds() / 3600
+
+                                    if hours_difference <= hours:
+                                        if data not in all_data:
+                                            all_data.append(data)
+
+                                except Exception as e:
+                                    continue
+
+                # Scroll down to load more content
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(5)  # Wait for new content to load
+
             output_box.delete('1.0', tk.END)  # Clear previous output
-            for post in all_data:
-                output_box.insert(tk.END, f"{post}\n\n")
+            output_box.insert(tk.END, "Viral Tweets\n\n")
 
-        except FileNotFoundError:
+            for i, data in enumerate(all_data, start=1):
+                output_box.insert(tk.END, f"{i}.\n")  # Number each tweet
+                for key, value in data.items():
+                    output_box.insert(tk.END, f"   {key.capitalize()}: {value}\n")
+                output_box.insert(tk.END, "\n")
+        else:
             driver.get(login)
             time.sleep(60)
             input_field = driver.find_element(by="name", value="text")
@@ -92,49 +155,183 @@ def start_scraping():
             time.sleep(90)
 
             cookies = driver.get_cookies()
-            with open('logs.txt', 'w') as f:
+            cookies_file_path = resource_path(f'Logs/{username}.txt')
+
+            # Open and write to the cookies file
+            with open(cookies_file_path, 'w') as f:
                 f.write(str(cookies))
-            driver.get(f"https://x.com/search?q={query} {country}&src=typed_query")
+            driver.get(search_query)
 
             # Add cookies to the driver
             for cookie in cookies:
                 driver.add_cookie(cookie)
             driver.refresh()
             time.sleep(5)
+            # driver.get('https://x.com/search?q=dog&src=typed_query')
+            time.sleep(20)
+            all_data = []
+            scroll_limit = 10  # Adjust this limit based on how many scrolls you want
 
-            # Parse the page with BeautifulSoup
+            for _ in range(scroll_limit):
+                # Use BeautifulSoup to parse page content
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                aria_labels = soup.find_all('div', {'role': 'group'})
+
+                for div in aria_labels:
+                    aria_label = div.get('aria-label')  # Get the aria-label attribute from each div
+                    if aria_label:  # Ensure aria-label exists
+                        data = {}
+                        for item in aria_label.split(', '):
+                            value, key = item.split(' ')
+                            if key in ['replies', 'reposts', 'likes', 'bookmarks', 'views']:
+                                data[key] = value
+                            else:
+                                try:
+                                    data[key] = int(value.replace('K', '000').replace('M', '000000'))
+                                except ValueError:
+                                    data[key] = 0  # Fallback in case of conversion error
+
+                        # Find post URL within parent div
+                        parent_div = div.find_parent('article')
+                        if parent_div:
+                            post_link = parent_div.find('a', {'role': 'link', 'href': True,
+                                                              'href': lambda href: '/status/' in href})
+                            data[
+                                'post_url'] = f"https://twitter.com{post_link['href']}" if post_link else 'No link found'
+
+                            # Now handle timestamp and filtering based on the hours
+                            tweets = driver.find_elements(By.CSS_SELECTOR, "article")
+                            for tweet in tweets:
+                                try:
+                                    timestamp_element = tweet.find_element(By.CSS_SELECTOR, "time")
+                                    tweet_time_str = timestamp_element.get_attribute("datetime")
+                                    tweet_time = datetime.strptime(tweet_time_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+                                    utc_time = pytz.utc.localize(tweet_time)
+
+                                    tweet_times = utc_time.astimezone(local_timezone)
+
+                                    # Calculate the time difference
+                                    time_difference = current_time - tweet_times.replace(tzinfo=None)
+
+                                    hours_difference = time_difference.total_seconds() / 3600
+
+                                    if hours_difference <= hours:
+                                        if data not in all_data:
+                                            all_data.append(data)
+
+                                except Exception as e:
+                                    continue
+
+                # Scroll down to load more content
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(5)  # Wait for new content to load
+
+            output_box.delete('1.0', tk.END)  # Clear previous output
+            output_box.insert(tk.END, "Viral Tweets\n\n")
+
+            for i, data in enumerate(all_data, start=1):
+                output_box.insert(tk.END, f"{i}.\n")  # Number each tweet
+                for key, value in data.items():
+                    output_box.insert(tk.END, f"   {key.capitalize()}: {value}\n")
+                output_box.insert(tk.END, "\n")
+
+
+
+    except FileNotFoundError:
+        driver.get(login)
+        time.sleep(60)
+        input_field = driver.find_element(by="name", value="text")
+        input_field.send_keys(username)
+        input_field.send_keys(Keys.ENTER)
+        time.sleep(10)
+
+        password_field = driver.find_element(by="name", value="password")
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.ENTER)
+        time.sleep(90)
+
+        cookies = driver.get_cookies()
+        cookies_file_path = resource_path(f'Logs/{username}.txt')
+
+        # Open and write to the cookies file
+        with open(cookies_file_path, 'w') as f:
+            f.write(str(cookies))
+        driver.get(search_query)
+
+        # Add cookies to the driver
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        driver.refresh()
+        time.sleep(5)
+        # driver.get('https://x.com/search?q=dog&src=typed_query')
+        time.sleep(20)
+        all_data = []
+        scroll_limit = 10  # Adjust this limit based on how many scrolls you want
+
+        for _ in range(scroll_limit):
+            # Use BeautifulSoup to parse page content
             soup = BeautifulSoup(driver.page_source, 'html.parser')
             aria_labels = soup.find_all('div', {'role': 'group'})
-            all_data = []
 
             for div in aria_labels:
-                aria_label = div.get('aria-label')
-                if aria_label:
+                aria_label = div.get('aria-label')  # Get the aria-label attribute from each div
+                if aria_label:  # Ensure aria-label exists
                     data = {}
                     for item in aria_label.split(', '):
                         value, key = item.split(' ')
                         if key in ['replies', 'reposts', 'likes', 'bookmarks', 'views']:
                             data[key] = value
+                        else:
+                            try:
+                                data[key] = int(value.replace('K', '000').replace('M', '000000'))
+                            except ValueError:
+                                data[key] = 0  # Fallback in case of conversion error
 
+                    # Find post URL within parent div
                     parent_div = div.find_parent('article')
                     if parent_div:
                         post_link = parent_div.find('a', {'role': 'link', 'href': True,
                                                           'href': lambda href: '/status/' in href})
-                        if post_link:
-                            post_url = post_link.get('href')
-                            full_url = f"https://twitter.com{post_url}"
-                            data['post_url'] = full_url
-                        else:
-                            data['post_url'] = 'No link found'
-                    all_data.append(data)
+                        data[
+                            'post_url'] = f"https://twitter.com{post_link['href']}" if post_link else 'No link found'
 
-            # Display the data in the output box
-            output_box.delete('1.0', tk.END)  # Clear previous output
-            for post in all_data:
-                output_box.insert(tk.END, f"{post}\n\n")
+                        # Now handle timestamp and filtering based on the hours
+                        tweets = driver.find_elements(By.CSS_SELECTOR, "article")
+                        for tweet in tweets:
+                            try:
+                                timestamp_element = tweet.find_element(By.CSS_SELECTOR, "time")
+                                tweet_time_str = timestamp_element.get_attribute("datetime")
+                                tweet_time = datetime.strptime(tweet_time_str, "%Y-%m-%dT%H:%M:%S.%fZ")
 
-        # Sleep for the specified interval (in seconds)
-        time.sleep(interval)
+                                utc_time = pytz.utc.localize(tweet_time)
+
+                                tweet_times = utc_time.astimezone(local_timezone)
+
+                                # Calculate the time difference
+                                time_difference = current_time - tweet_times.replace(tzinfo=None)
+
+                                hours_difference = time_difference.total_seconds() / 3600
+
+                                if hours_difference <= hours:
+                                    if data not in all_data:
+                                        all_data.append(data)
+
+                            except Exception as e:
+                                continue
+
+            # Scroll down to load more content
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(5)  # Wait for new content to load
+
+        output_box.delete('1.0', tk.END)  # Clear previous output
+        output_box.insert(tk.END, "Viral Tweets\n\n")
+
+        for i, data in enumerate(all_data, start=1):
+            output_box.insert(tk.END, f"{i}.\n")  # Number each tweet
+            for key, value in data.items():
+                output_box.insert(tk.END, f"   {key.capitalize()}: {value}\n")
+            output_box.insert(tk.END, "\n")
 
 
 # Function to start scraping in a new thread
